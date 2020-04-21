cd6f55ff4570fcbca15633ba26877e37fb7ed984
Dor Genosar <dor.genosar@outlook.com>
2020-04-21 23:05:07 +0300

    Added guild reference to conference object

--- munbot/core/conference.py
+++ munbot/core/conference.py
@@ -2,8 +2,9 @@ import discord
 
 
 class Conference(object):
-    def __init__(self, name: str):
+    def __init__(self, name: str, guild: discord.Guild):
         self._name = name
+        self.guild = guild
         self._committees = dict()
     
     @property
@@ -19,3 +20,9 @@ class Conference(object):
             self._committees[committee.name] = committee
         else:
             raise KeyError(f'A {committee.name} committee is already registered!')
+    
+    def remove_committee(self, committee_name):
+        if committee_name in self._committees:
+            self._committees.pop(committee_name)
+        else:
+            raise KeyError(f'A {committee_name} committee was never registered to this conference!')
