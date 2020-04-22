7b26d6184f998f92d838c71ae5fb59db59f87efd
Dor Genosar <dor.genosar@outlook.com>
2020-04-22 23:43:39 +0300

    Added committee initialization and finalization to Conference's register and remove committee functions, respectively

--- munbot/core/conference.py
+++ munbot/core/conference.py
@@ -1,5 +1,7 @@
 import discord
 
+from munbot.core.exceptions import InvalidCommittee
+
 
 class Conference(object):
     def __init__(self, name: str, guild: discord.Guild):
@@ -15,14 +17,28 @@ class Conference(object):
     def committees(self):
         return self._committees.values()
     
-    def register_committee(self, committee):
+    async def register_committee(self, committee):
         if not committee.name in self._committees:
             self._committees[committee.name] = committee
+            await committee.initialize_committee()
         else:
-            raise KeyError(f'A {committee.name} committee is already registered!')
+            raise InvalidCommittee(f'A {committee.name} committee is already registered to the {self.name} conference!')
     
-    def remove_committee(self, committee_name):
+    async def remove_committee(self, committee_name):
         if committee_name in self._committees:
-            self._committees.pop(committee_name)
+            committee = self._committees.pop(committee_name)
+            await committee.finalize_committee()
         else:
-            raise KeyError(f'A {committee_name} committee was never registered to this conference!')
+            raise InvalidCommittee(f'A {committee_name} committee was never registered to the {self.name} conference!')
+    
+    def get_committee_by_name(self, committee_name):
+        if committee_name in self._committees:
+            return self._committees[committee_name]
+        else:
+            raise InvalidCommittee(f'A {committee_name} committee was never registered to the {self.name} conference!')
+    
+    async def register_delegation(self, committee_name, delegation):
+        await self.get_committee_by_name(committee_name).add_delegation(delegation)
+        
+    async def remove_delegation(self, committee_name, delegation_country):
+        await self.get_committee_by_name(committee_name).remove_delegation(delegation_country)
