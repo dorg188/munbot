301b55cc209fa97af9d4368c00ae848b584afa5f
Dor Genosar <dor.genosar@outlook.com>
2020-04-22 23:41:53 +0300

    Added custom exceptions

---
+++ munbot/core/exceptions.py
@@ -0,0 +1,14 @@
+class MUNException(Exception):
+    pass
+
+
+class InvalidCommittee(MUNException):
+    pass
+
+
+class InvalidDelegation(MUNException):
+    pass
+
+
+class InvalidCommitteeState(MUNException):
+    pass
